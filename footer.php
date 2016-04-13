cc688c2f3c16024b7eba1c99fb4fd3c412044939
thekirankumardash <thekirankumardash@gmail.com>
2016-04-13 21:25:24 +0530

    45. Create Your Underscores Starter Theme

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package iteach
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'iteach' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'iteach' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'iteach' ), 'iteach', '<a href="http://www.bgwebagency.com/" rel="designer">Kiran Kumar Dash</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
