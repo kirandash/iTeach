22981b17ddb88e76c2fd14e2e3bd231b03acbdcb
thekirankumardash <thekirankumardash@gmail.com>
2016-04-14 10:33:36 +0530

    49. Cover Your Assets (Adding Your Styles, Scripts, Fonts & Images)

--- header.php
+++ header.php
@@ -17,7 +17,24 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!-- Bootstrap core CSS -->
+<link href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- FontAwesome Icons -->
+<link href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        
+<!-- Google Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+
 <?php wp_head(); ?>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
