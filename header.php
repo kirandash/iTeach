ef031aa8fbbcb6a8e06fedc37eecb74143c5722e
thekirankumardash <thekirankumardash@gmail.com>
2016-04-14 10:53:51 +0530

    50. Clean Up Your Stylesheet & Modify Your CSS for WordPress

--- header.php
+++ header.php
@@ -53,13 +53,13 @@
                   <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class="navbar-header">
-                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collaps">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                       </button>
-                      <a class="navbar-brand" href="/"><img src="assets/img/logo.png" alt="iTeach WordPress"></a>
+                      <a class="navbar-brand" href="/"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/img/logo.png" alt="iTeach WordPress"></a>
                     </div><!-- navbar-header -->
                 
                     <?php
