91c9858dc75654a8de3520c5ef76c2c14fc405e7
thekirankumardash <thekirankumardash@gmail.com>
2016-04-20 22:40:10 +0530

    65. How to Add a Resource and Display It In Your Custom Theme

--- page-resources.php
+++ page-resources.php
@@ -5,7 +5,8 @@
 
 get_header(); 
 
-$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->id ) );
+$thumbnail_url		 = wp_get_attachment_url( get_post_thumbnail_id( $post->id ) );
+
 ?>
 
         <!-- FEATURE IMAGE
@@ -33,47 +34,40 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->id ) );
                     
                     <section class="main-content">
                                             
-                            <p class="lead">Welcome to the Bootstrap to WordPress resources page! On this page, I will provide you with useful resources, plugins, tools, applications, and websites to help make your life as a coder a little bit easier. <strong>I recommend adding this page to your bookmarks for convenient future reference.</strong></p>
-    <p><em>Please keep in mind that some of the following resources are my affiliate links, and should you decide to make a purchase, I will earn a small commission — at no extra cost to you! I am recommending these resources only because I have found them to be incredibly helpful. Don’t spend your money on any products or resources unless you think they will benefit you.</em></p>
+                            <?php while( have_posts() ): the_post(); ?>
+								 
+                                 <?php the_content(); ?>
+                                 
+							<?php endwhile; // end of the loop ?>
                                 
+                        <?php $loop = new WP_Query( array( 'post_type' => 'resource', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+                        
                         <hr>
                         
-                        <div class="resource-row clearfix">         
-                                                    
+                        <div class="resource-row clearfix">   
+                        
+                        	<?php while( $loop->have_posts() ): $loop->the_post();    
+
+                            $resource_image		 = get_field('resource_image');
+                            $resource_url		 = get_field('resource_url');
+                            $add_button			 = get_field('add_button');
+                            $button_text		 = get_field('button_text');
+							
+							?>
+                            
                             <div class="resource">
     
-                                <img src="assets/img/justhost-logo.jpg" alt="Justhost Logo">
-                                
-                                
-                                <h3><a href="http://kirandash.in/justhost">Justhost</a></h3>
-                                <p>When I made my very first website about 7 years ago, I signed up with Justhost — and I’ve never looked back! Since then, I’ve built hundreds of websites with Justhost as my only hosting provider. I recommend these guys to all of my clients, my students, my family and friends.</p>
-    <p>If you’re looking to start your very first website, or are looking for a reliable hosting provider — look no further. You get a free domain name for life, great customer service, unlimited domains, unlimited email accounts, unlimited GBs of space, an anytime money back guarantee and my favourite part: You can set up a blog website in minutes with 1-click WordPress Install. These guys are awesome! Get started with Justhost.</p>
+                                <img src="<?php echo $resource_image['url']; ?>" alt="<?php echo $resource_image['alt']; ?>" >                      
+                                <h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+                                <?php the_content(); ?>
                             
-                                                            <a href="http://kirandash.in/justhost" class="btn btn-success">Get started with Justhost</a>
+                                <?php if( !empty($button_text) ): ?>
+                                <a href="<?php echo $resource_url; ?>" class="btn btn-success"><?php echo $button_text; ?></a>
+                                <?php endif; ?>
                             
-                            </div><!-- resource -->   
-                                                    
-                            <div class="resource">
-    
-                                <img src="assets/img/coda2-logo.jpg" alt="Coda 2 Logo">
-                                
-                                <h3><a href="http://kirandash.in/coda2">Coda 2</a></h3>
-                                <p>If you’re serious about coding, or you want to start out with a solid tool to code with, I strongly recommend Coda 2 for Mac. A beautiful User Interface with tabbed navigation, a super sidebar, and a simple layout. The features are outrageous and extensive, with code folding, smart complete, auto indentation, GIT integration, a built-in terminal, iCloud support — to name a few. The features seem to be endless.</p>
-    <p>I use Coda 2 for all of my courses &amp; tutorials, and if you decide to use Coda 2 as your editor of choice, it would greatly benefit you when taking my courses. The price of the editor weighs in at $75, but it’s worth every penny — in my honest opinion.</p>
+                            </div><!-- resource -->
                             
-                              <a href="http://kirandash.in/coda2" class="btn btn-success">Check out Coda 2</a>
-                            </div><!-- resource -->       
-                                                    
-                            <div class="resource">
-    
-                                <img src="assets/img/dropbox-logo.jpg" alt="Dropbox Logo">
-                                
-                                <h3><a href="http://kirandash.in/dropbox">Dropbox</a></h3>
-                                <p>This tool should go without saying. But if I have to explain — you <strong>need</strong> this tool. I use it 24/7 — literally. Dropbox is where I store my personal &amp; business related data. It&#8217;s accessible anywhere, and regularly backs up my important information.</p>
-    <p>Because my data is accessible anywhere, Dropbox allows me to work on a client project at home, on the bus, in a coffee shop, or at a friend&#8217;s house. It&#8217;s truly a gem of a tool, and I think you&#8217;ll believe me.</p>
-                            
-                                <a href="http://kirandash.in/dropbox" class="btn btn-success">Get 2GB in Dropbox for free</a>
-                           </div><!-- resource -->
+                            <?php endwhile; ?>
                               
                         </div><!-- resource-row -->
                         
