fae725bd8a519250e81f5d36a268d430adfd0c70
thekirankumardash <thekirankumardash@gmail.com>
2016-04-20 21:35:35 +0530

    64. Add a Feature Image, and a Fallback Image

--- page-resources.php
+++ page-resources.php
@@ -3,13 +3,26 @@
  * Template Name: Resources Page
  */
 
-get_header(); ?>
+get_header(); 
+
+$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->id ) );
+?>
 
         <!-- FEATURE IMAGE
         ========================================================= -->         
-        <section class="feature-image feature-image-default" data-type="background" data-speed="2">
-            <h1 class="page-title">Resources</h1>
+        <?php if( has_post_thumbnail() ){// check for feature image ?>
+        
+        <section class="feature-image" style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
+            <h1 class="page-title"><?php the_title(); ?></h1>
         </section>
+        
+        <?php } else { ?>
+		
+        <section class="feature-image feature-image-default" data-type="background" data-speed="2">
+            <h1 class="page-title"><?php the_title(); ?></h1>
+        </section>
+        	
+		<?php } ?>
 
         <!-- MAIN CONTENT
         ================================================== -->
