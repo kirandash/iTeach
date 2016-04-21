15a86bec86583d67b2b13d3c50e9af7395144adc
thekirankumardash <thekirankumardash@gmail.com>
2016-04-21 21:38:17 +0530

    START Section (14) - Convert The Blog. 66. Easily Set Up Your Custom Blog Index Page

--- index.php
+++ index.php
@@ -14,43 +14,59 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+        <h1 class="page-title">Blog</h1>
+    </section>
+    
+    <div class="container">
+            <div class="row" id="primary">
+                    
+                <main id="content" class="col-sm-8" role="main">
 
-		<?php
-		if ( have_posts() ) :
+					<?php
+                    if ( have_posts() ) :
+            
+                        if ( is_home() && ! is_front_page() ) : ?>
+                            <header>
+                                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                            </header>
+            
+                        <?php
+                        endif;
+            
+                        /* Start the Loop */
+                        while ( have_posts() ) : the_post();
+            
+                            /*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            get_template_part( 'template-parts/content', get_post_format() );
+            
+                        endwhile;
+            
+                        the_posts_navigation();
+            
+						else :
+				
+							get_template_part( 'template-parts/content', 'none' );
+				
+						endif; ?>                
+                </main><!-- content -->
+                
+                <!-- SIDEBAR
+                ================================================== -->
+                <aside class="col-sm-4">
+    
+                    <aside class="widget">
+                        <?php get_sidebar(); ?>
+                    </aside>
+                            
+                </aside>
+                
+            </div><!-- primary -->
+    </div><!-- container -->
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
+<?php 
 get_footer();
