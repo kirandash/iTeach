85e4e0aaa5f4b5e8fd22d85d29a85fa8819e6576
thekirankumardash <thekirankumardash@gmail.com>
2016-04-22 07:42:12 +0530

    67. Code Your Individual Blog Excerpts

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,27 +19,32 @@
 			}
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php iteach_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		
+        <div class="post-details">
+            <i class="fa fa-user"></i> <?php the_author(); ?>			
+            <i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+            <i class="fa fa-folder"></i> <?php the_category( ', ' ); ?>			
+            <i class="fa fa-tags"></i> <?php the_tags(); ?>	
+            <div class="post-comments-badge">
+                <a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number( 0, 1 , '%' ); ?></a>
+            </div><!-- post-comments-badge -->
+            <?php edit_post_link( 'Edit', '<i class="fa fa-pencil"></i>', ''); ?>
+         </div><!-- post-details -->
+         
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'iteach' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+	<?php if( has_post_thumbnail() ) { ?>
+    <div class="post-image">
+        <?php the_post_thumbnail(); ?>	
+    </div><!-- post-image -->
+	<?php } ?>
+        
+    <div class="post-excerpt">
+        <?php the_excerpt(); ?>
+    </div><!-- post-excerpt -->
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'iteach' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php iteach_entry_footer(); ?>
