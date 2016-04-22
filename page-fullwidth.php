1ceedc65c0d7a97be0474add4bd849792f649e74
thekirankumardash <thekirankumardash@gmail.com>
2016-04-22 21:57:30 +0530

    START Section (15) - Convert Contact Page 72. Create & Customize a Full Width Template

---
+++ page-fullwidth.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Full-Width Page
+ */
+
+$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->id ) );
+get_header(); 
+
+?>
+
+<?php if( has_post_thumbnail() ) { ?>
+<section class="feature-image feature-image-default" style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
+    <h1 class="page-title"><?php the_title(); ?></h1>
+</section>
+
+<?php }else { ?>
+<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+    <h1 class="page-title"><?php the_title(); ?></h1>
+</section>	
+<?php } ?>
+
+
+<div class="container">
+    <div class="row" id="primary">
+        <div id="content" class="col-sm-12">
+            
+            <section class="main-content">
+                                    
+                    <?php while( have_posts() ): the_post(); ?>
+                    	
+                        <?php the_content(); ?>
+                        
+                    <?php endwhile; ?>
+                                
+            </section><!-- main-content -->
+            
+        </div><!-- content -->
+    </div><!-- row -->
+</div><!-- container -->
+
+
+<?php get_footer(); ?>
