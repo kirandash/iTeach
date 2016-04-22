f0bfb10dcca4c61b685e1d5f49bc5acd37e42e31
thekirankumardash <thekirankumardash@gmail.com>
2016-04-22 21:30:42 +0530

    71. Customize the Search Results Template

--- search.php
+++ search.php
@@ -9,39 +9,55 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+            <h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'iteach' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+        </section>
+			
+				
+	<div class="container">
+    	<div id="primary" class="row">	
 
-		<?php
-		if ( have_posts() ) : ?>
+			<main id="content" class="col-sm-8">
+            
+				<?php
+                if ( have_posts() ) : ?>
+        
+                    
+        
+                    <?php
+                    /* Start the Loop */
+                    while ( have_posts() ) : the_post();
+        
+                        /**
+                         * Run the loop for the search to output the results.
+                         * If you want to overload this in a child theme then include a file
+                         * called content-search.php and that will be used instead.
+                         */
+                        get_template_part( 'template-parts/content', 'search' );
+        
+                    endwhile;
+        
+                    the_posts_navigation();
+        
+                else :
+        
+                    get_template_part( 'template-parts/content', 'none' );
+        
+                endif; ?>
+        
+                </main><!-- #content -->
+                
+              <!-- SIDEBAR
+              ================================================== -->
+              <aside class="col-sm-4">
+            
+                <?php get_sidebar(); ?>
+                        
+              </aside>
+                                  
+           </div><!-- #primary -->
+      </div><!-- .container -->
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'iteach' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
 
 <?php
 get_sidebar();
